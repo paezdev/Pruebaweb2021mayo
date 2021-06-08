7a026840290b00090d35300024af433a0f63b418
zeap83 <31019816+zeap83@users.noreply.github.com>
2021-06-08 17:41:41 -0500

    Delete WebDjango directory

--- WebDjango/Proyecto1/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def saludo(resquest): #mi primera vista
-    return HttpResponse('Hola profe, aquí presentando nuestra primera web desde el framework de Django y subiendolo a GitHub')
