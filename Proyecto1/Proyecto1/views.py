9069bc86900c08b075174cbe81c3317501737d19
zeap83 <31019816+zeap83@users.noreply.github.com>
2021-06-08 23:51:55 -0500

    Delete Proyecto1 directory

--- Proyecto1/Proyecto1/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def saludo(request): # mi primera vista
-
-    return HttpResponse("Hola profe, aquí presentando nuestra primera vista creada con el framework de Django y subiendolo a GitHub")
